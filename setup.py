9383c4fd38436b01c4abbbda34e814ee2c574fb3
Changsong Ding <memsding@gmail.com>
2019-10-16 11:52:44 -0700

    Added package files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='citSampler',
+    version='1.0.0',
+    packages=['citSampler'],
+    url='',
+    license='',
+    author='Changsong Ding',
+    author_email='memsding@gmail.com',
+    description='the Sampler'
+)
